bf48a2f8fc9f8578c14b96c1e6f757eaf21c29ee
vijeyash <vijeyash@gmail.com>
2024-06-25 12:06:24 +0530

    added anagram

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -5,6 +5,10 @@ import "fmt"
 // anagram is word formed for rearranging exactly the same word using same letters.
 
 func FindAnagram(word, target string) {
+	if len(word) != len(target) {
+		fmt.Println("its not a anagram")
+		return
+	}
 	t := make(map[rune]int)
 	for _, v := range word {
 		t[v]++
