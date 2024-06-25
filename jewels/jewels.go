40eec248007e53e550dc2a7ca9b3de2f65697bf5
vijeyash <vijeyash@gmail.com>
2024-06-25 16:35:28 +0530

    jewelsandstones

--- jewels/jewels.go
+++ jewels/jewels.go
@@ -2,9 +2,11 @@ package jewels
 
 import "fmt"
 
-//we will be given stones that are jewels and stones which represents
-//  all stones we have. we need to find how many jewels we have
-
+// we will be given stones that are jewels and stones which represents
+//
+//	all stones we have. we need to find how many jewels we have
+//
+// quadratic time complexity
 func FindJewels(jewels string, stones string) {
 	count := 0
 	for _, j := range jewels {
@@ -15,4 +17,21 @@ func FindJewels(jewels string, stones string) {
 		}
 	}
 	fmt.Printf("total jewels we have is %d", count)
+}
+
+// Linear time complexity
+func FindJewelss(jewels string, stones string) {
+	count := 0
+	jewelSet := make(map[rune]bool)
+	for _, j := range jewels {
+		jewelSet[j] = true
+	}
+
+	for _, s := range stones {
+		if jewelSet[s] {
+			count++
+		}
+	}
+	fmt.Printf("total jewels we have is %d", count)
+
 }
